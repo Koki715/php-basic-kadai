96e0f69019260e63c9d3713dcc2f5f31724e0b89
Koki715 <foreverbrightness715@gmail.com>
2025-05-07 16:43:37 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <p>
+        <?php
+        $item_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($item_data);
+        ?>
+    </p>
+</body>
+</html>
